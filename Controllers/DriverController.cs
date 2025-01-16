53a408df4d626aa09737195992bd5c848405ef8a
kosangp <poudelkoshang@gmail.com>
2025-01-16 12:42:06 +0545

    passenger list updated to driver

--- Controllers/DriverController.cs
+++ Controllers/DriverController.cs
@@ -1,14 +1,45 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using YatriSewa.Models;
 
 namespace YatriSewa.Controllers
 {
     [Authorize(Roles = "Driver")]
     public class DriverController : Controller
     {
+        private readonly ApplicationContext _context;
+
+        public DriverController(ApplicationContext context)
+        {
+            _context = context;
+        }
         public IActionResult DriverDashboard()
         {
             return View();
         }
+        public IActionResult Journey()
+        {
+            return View();
+        }
+        public IActionResult PassengerList(int busId)
+        {
+            // Assuming Role is an enum of type 'UserRole'
+            var passengers = _context.User_Table
+                .Where(u=> u.Role == UserRole.Passenger) // Use the enum value for comparison
+                .Select(u => new User
+                {
+                   UserId = u.UserId,
+                   Name= u.Name,
+                   Email= u.Email,
+                   Password= u.Password,
+                   PhoneNo = u.PhoneNo /*u.PhoneNumber*/
+                })
+                .ToList();
+
+            return View(passengers);
+        }
+
+
     }
 }
